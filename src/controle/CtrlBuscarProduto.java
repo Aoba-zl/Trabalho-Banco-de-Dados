cb2aa134b24973b693fb0de7da5a47f3ca2edb89
DeboraCristina <deboracristinaproficional1@gmail.com>
2023-10-15 20:24:42 -0300

    controles adicionados. 70% dos metodos prototipados.

---
+++ src/controle/CtrlBuscarProduto.java
@@ -0,0 +1,50 @@
+package controle;
+
+import modelo.Carrinho;
+import modelo.Cliente;
+import modelo.Item;
+import modelo.Produto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CtrlBuscarProduto
+{
+    Cliente cliente;
+    Carrinho carrinho;
+
+    public CtrlBuscarProduto(Cliente cliente)
+    {
+        this.cliente = cliente;
+        this.carrinho = cliente.getCarrinho();
+    }
+
+    public List<Produto> buscarProduto(String nome)
+    {
+        List<Produto> produtosEncontrados = new ArrayList<Produto>();
+        //TODO: CtrlBuscarProduto. Corpo da operacao
+        return produtosEncontrados;
+    }
+    public void adicionarAoCarrinho(Produto produto, int quantidade)
+    {
+        //TODO: CtrlBuscarProduto. Corpo da operacao
+        Item novoItem = gerarItemCarrinhoPedido(produto, quantidade);
+        carrinho.addItem(novoItem);
+    }
+    public void adicionarAoCarrinhoComoPedido(Produto produto, int quantidade)
+    {
+        //TODO: CtrlBuscarProduto. Corpo da operacao. se não der para
+        // envivar o produto já selecionado, esse método morre
+        Item novoItem = gerarItemCarrinhoPedido(produto, quantidade);
+        carrinho.addItem(novoItem);
+    }
+    private Item gerarItemCarrinhoPedido (Produto produto, int quantidade)
+    {
+        return new Item(produto, quantidade);
+    }
+    //TODO: há resalval em relação a essa opecacão ficar no controle
+    public void exibirProduto(Produto produto)
+    {
+        //TODO: CtrlBuscarProduto. Corpo da operacao
+    }
+}
