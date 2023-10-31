bccd64652b9724fb764c627ea6428da22a28f097
Matheus França <matheussss6613@gmail.com>
2023-10-31 20:58:46 -0300

    Update LoginController.java

--- src/control/LoginController.java
+++ src/control/LoginController.java
@@ -1,6 +1,26 @@
 package control;
 
-public class LoginController 
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.scene.control.TextField;
+
+public class LoginController implements EventHandler<Event>
 {
+	private TextField tfUserName;
+	private TextField tfpassword;
+	
+	public LoginController(TextField tfUserName, TextField tfPassword)
+	{
+		this.tfUserName = tfUserName;
+		this.tfpassword = tfPassword;
+	}
+
+	@Override
+	public void handle(Event event) 
+	{
+		
+	}
+	
+	
 	
 }
