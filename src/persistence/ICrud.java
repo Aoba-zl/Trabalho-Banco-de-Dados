ba5cb9643153f6191db0b0c014e5a4f2c78d34b7
Matheus França <matheussss6613@gmail.com>
2023-10-31 20:58:39 -0300

    Update ICrud.java

--- src/persistence/ICrud.java
+++ src/persistence/ICrud.java
@@ -3,7 +3,7 @@ package persistence;
 import java.sql.SQLException;
 import java.util.List;
 
-public interface ICrud<T> 
+public interface ICrud<T> //optei por não utilizar por motivos de que eu não sei porque de fato iria utilizar isso @_@
 {
 	public void insert (T t) throws SQLException;
 	public void update (T t) throws SQLException;
