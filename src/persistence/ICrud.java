91b2d09942c900dacb10285436da26b31465c3c2
Matheus França <matheussss6613@gmail.com>
2023-11-02 07:31:38 -0300

    alt void -> boolean

--- src/persistence/ICrud.java
+++ src/persistence/ICrud.java
@@ -5,9 +5,9 @@ import java.util.List;
 
 public interface ICrud<T> //optei por não utilizar por motivos de que eu não sei porque de fato iria utilizar isso @_@
 {
-	public void insert (T t) throws SQLException;
-	public void update (T t) throws SQLException;
-	public void delete (T t) throws SQLException;
+	public boolean insert (T t) throws SQLException;
+	public boolean update (T t) throws SQLException;
+	public boolean delete (T t) throws SQLException;
 	public T consult (T t) throws SQLException;
 	public List<T> list() throws SQLException;
 	
