142f1e032cf35bf19cf4597b1a89581ed67d8ade
Matheus França <matheussss6613@gmail.com>
2023-11-03 23:35:39 -0300

    drop import

--- src/view/WinRegProductConstructor.java
+++ src/view/WinRegProductConstructor.java
@@ -12,7 +12,6 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 
 public class WinRegProductConstructor 
