3a6ebd21dc9a59e04b1343129cbd18118f1c0695
DeboraCristina <deboracristinaproficional1@gmail.com>
2023-11-05 19:20:53 -0300

    tabela de endereços puxando direto do Banco - ajuste

--- src/model/ClientAddress.java
+++ src/model/ClientAddress.java
@@ -11,4 +11,17 @@ public class ClientAddress extends Address
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	@Override
+	public String toString()
+	{
+		String name = getName();
+		String cep = getCep();
+		String number = Integer.toString(getDoorNumber());
+		String comp = getComplement();
+		
+		String msg = String.format("%s;%s;%s;%s", name, cep, number, comp);
+		
+		return msg;
+	}
 }
