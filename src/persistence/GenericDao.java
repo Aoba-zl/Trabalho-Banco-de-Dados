abc68dd250fafdda3398dcb5aed42e94a1fa54c4
Luan Camilo Nogueira <luancamilo313@gmail.com>
2023-11-06 18:13:10 -0300

    GenericDao atulizado pra o intelijj

--- src/persistence/GenericDao.java
+++ src/persistence/GenericDao.java
@@ -1,25 +1,24 @@
 package persistence;
 
 import java.sql.Connection;
+import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class GenericDao 
-{
-	private Connection c;
-	
-	public Connection getConnection() throws SQLException
-	{
-		String hostName = "localHost";
-		String dbName = "marketplace";
-		String user = "usuarioDB";
-		String password = "123456";
-		
-		c = DriverManager.getConnection(String.format("jdbc:jtds:sqlserver://%s:1433;databaseName=%s;user=%s;password=%s", 
-										hostName, dbName, user, password));
-		
-		return c;
-	}
-	
-	
+public class GenericDao {
+    private static final String URL= "jdbc:sqlserver://localhost\\DESKTOP-GFU36L4:1433;databaseName=marketplace;instance=MSSQLSERVER;encrypt=false;trustServerCertificate=true;";
+    private static final String user= "usuarioDB";
+    private static final String password= "123456";
+
+    Connection connection= null;
+    public Connection getConnection() throws SQLException {
+        try {
+            connection= DriverManager.getConnection(URL, user, password);
+            System.out.println("Banco de Dados conectado com sucesso!");
+        } catch (SQLException e){
+            System.out.println("Falha ao conectar com o Banco de Dados \n" + e.getMessage());
+        }
+        return connection;
+    }
+
 }
