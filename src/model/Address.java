23aeb8a483d0f0ff0db48f1080149989f8bc05ec
DeboraCristina <deboracristinaproficional1@gmail.com>
2023-11-25 01:17:41 -0300

    javaDoc add

--- src/model/Address.java
+++ src/model/Address.java
@@ -1,4 +1,7 @@
 package model;
+/**
+ * Classe Modelo que representa um endereço.
+ */
 public class Address
 {
 	private String cep;
@@ -8,7 +11,18 @@ public class Address
 	private String logradouro;
     private int doorNumber;
     private String complement;
-    
+
+	/**
+	 * Construtor para inicializar um objeto Address com os detalhes do endereço.
+	 *
+	 * @param cep        CEP do endereço.
+	 * @param logradouro Logradouro.
+	 * @param complement Complemento do endereço.
+	 * @param bairro     Bairro do endereço.
+	 * @param uf         Estado do endereço.
+	 * @param localidade Cidade do endereço.
+	 * @param doorNmbr   Número de porta.
+	 */
 	public Address(String cep,String logradouro,String complement,String bairro, String uf, String localidade, String doorNmbr)
 	{
 		this.setCep(cep);	
@@ -20,50 +34,123 @@ public class Address
 		this.setDoorNumber(Integer.parseInt(doorNmbr));
 
 	}
+	/**
+	 * Construtor vazio para criar uma instância de Address.
+	 */
 	public Address()
 	{
 		super();
 	}
 
+	/**
+	 * Obtém o CEP do endereço.
+	 *
+	 * @return O CEP do endereço.
+	 */
 	public String getCep() {
 		return cep;
 	}
+	/**
+	 * Define o CEP do endereço.
+	 *
+	 * @param cep O CEP do endereço.
+	 */
 	public void setCep(String cep) {
 		this.cep = cep;
 	}
+	/**
+	 * Obtém o estado do endereço.
+	 *
+	 * @return O estado do endereço.
+	 */
 	public String getEstate() {
 		return uf;
 	}
+	/**
+	 * Define o estado do endereço.
+	 *
+	 * @param estate O estado do endereço.
+	 */
 	public void setEstate(String estate) {
 		this.uf = estate;
 	}
+	/**
+	 * Obtém a cidade do endereço.
+	 *
+	 * @return A cidade do endereço.
+	 */
 	public String getCity() {
 		return localidade;
 	}
+	/**
+	 * Define a cidade do endereço.
+	 *
+	 * @param city A cidade do endereço.
+	 */
 	public void setCity(String city) {
 		this.localidade = city;
 	}
+	/**
+	 * Obtém o bairro do endereço.
+	 *
+	 * @return O bairro do endereço.
+	 */
 	public String getNeighborhood() {
 		return bairro;
 	}
+	/**
+	 * Define o bairro do endereço.
+	 *
+	 * @param bairro O bairro do endereço.
+	 */
 	public void setNeighborhood(String bairro) {
 		this.bairro = bairro;
 	}
+	/**
+	 * Obtém o logradouro do endereço.
+	 *
+	 * @return O logradouro do endereço.
+	 */
 	public String getStreet() {
 		return logradouro;
 	}
+	/**
+	 * Define o logradouro do endereço.
+	 *
+	 * @param logradouro O logradouro do endereço.
+	 */
 	public void setStreet(String logradouro) {
 		this.logradouro = logradouro;
 	}
+	/**
+	 * Obtém o complemento do endereço.
+	 *
+	 * @return O complemento do endereço.
+	 */
 	public String getComplement() {
 		return complement;
 	}
+	/**
+	 * Define o complemento do endereço.
+	 *
+	 * @param complement O complemento do endereço.
+	 */
 	public void setComplement(String complement) {
 		this.complement = complement;
 	}
+	/**
+	 * Obtém o número de porta do endereço.
+	 *
+	 * @return O número de porta do endereço.
+	 */
 	public int getDoorNumber() {
 		return doorNumber;
 	}
+	/**
+	 * Define o número de porta do endereço.
+	 *
+	 * @param doorNumber O número de porta do endereço.
+	 */
 	public void setDoorNumber(int doorNumber) {
 		this.doorNumber = doorNumber;
 	}
--- src/model/Cart.java
+++ src/model/Cart.java
@@ -2,31 +2,59 @@ package model;
 
 import java.util.List;
 
+/**
+ * Classe Modelo que representa um carrinho de compras.
+ */
 public class Cart
 {
     private List<Item> items;
 	private double total;
 
+    /**
+     * Obtém a lista de itens no carrinho.
+     *
+     * @return A lista de itens no carrinho.
+     */
     public List<Item> getItens()
 	{
         return items;
     }
 
+    /**
+     * Define a lista de itens no carrinho.
+     *
+     * @param items A lista de itens no carrinho.
+     */
     public void setItems(List<Item> items)
 	{
         this.items = items;
     }
-    
+
+    /**
+     * Adiciona um novo item ao carrinho.
+     *
+     * @param newProduct O novo item a ser adicionado ao carrinho.
+     */
     public void addItems(Item newProduct)
     {
         this.items.add(newProduct);
     }
 
+    /**
+     * Obtém o total do carrinho.
+     *
+     * @return O total do carrinho.
+     */
     public double getTotal()
 	{
         return total;
     }
 
+    /**
+     * Define o total do carrinho.
+     *
+     * @param total O total do carrinho.
+     */
     public void setTotal(double total)
 	{
         this.total = total;
